77c0c65d2877b6551e99322d6d5f6852406833de
Ryan Rawdon <ryan@u13.net>
2016-06-12 20:25:16 +0000

    Added daemonization

--- agent/agent.py
+++ agent/agent.py
@@ -1,7 +1,30 @@
 #! /usr/bin/env python3
-
-
 from agent import APIClient
+import argparse
+from daemonize import Daemonize
+#import logger
 
-a = APIClient.APIClient('http://199.187.221.170:5000/api/')
-a.register()
+HOLDTIME=60
+DELAY=5
+
+pid = "/tmp/picon-agent.pid"
+
+parser = argparse.ArgumentParser(description = 'Run a PiCon agent')
+parser.add_argument('-d',dest='daemonize',action='store_true',help="Run in background [default: run in foreground]")
+
+args = parser.parse_args()
+
+
+def main():
+    a = APIClient.APIClient('http://199.187.221.170:5000/api/',holdtime=HOLDTIME,delay=DELAY)
+    a.run()
+
+
+if args.daemonize:
+    daemon = Daemonize(app="picon-agent", pid=pid, action=main)
+    daemon.start()
+else:
+    main()
+
+
+
--- agent/agent/APIClient.py
+++ agent/agent/APIClient.py
@@ -1,25 +1,33 @@
 from .utils import *
+from time import sleep
 
 class APIClient():
-    def __init__(self,endpoint='http://localhost/api/',headers={'content-type': 'application/json'}):
+    def __init__(self,endpoint='http://localhost/api/',headers={'content-type': 'application/json'},holdtime=300,delay=60):
         self.endpoint = endpoint
         self.headers = headers
+        self.holdtime = holdtime
+        self.delay = delay
     def register(self):
         body = {}
         body['hostname'] = getHostname()
         body['sn'] = getSerial()
         body['interfaces'] = getInterfaces()
         body['ports'] = getPorts()
+        body['holdtime'] = self.holdtime
         jsonbody = json.dumps(body,sort_keys=True,indent=2)
         print(self.endpoint+'register')
         requests.post(self.endpoint+'register', data = jsonbody, headers = self.headers)
-
+    def run(self):
+        while True:
+            self.register()
+            sleep(self.delay)
 
 def main():
     # create an agent and register
     a = APIClient('http://199.187.221.170:5000/api/')
     a.register()
     sys.stderr.write(APIClient.jsonbody)
+
 if __name__ == "__main__":
     main()
 
