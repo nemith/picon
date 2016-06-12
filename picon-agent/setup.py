988ba63f48b7e99a46cfc30861c5559cc2066cf9
Ryan Rawdon <ryan@u13.net>
2016-06-12 22:43:12 +0000

    Misc setup changes

--- picon-agent/setup.py
+++ picon-agent/setup.py
@@ -2,27 +2,18 @@
 import setuptools
 import os
 
-from  setuputils import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+pkgs = setuptools.find_packages()
 
 setuptools.setup(
     name = "PiCon Registration Agent",
     version = "0.0.1",
     author = "Team Kickass",
     author_email = "ryan@u13.net",
-    description = ("PiCon registration agent for the PiCon console registry"),
+    description = "PiCon registration agent for the PiCon console registry",
     license = "BSD",
     keywords = "RaspberryPi Terminal Server Console",
     url = "http://nanog.org",
-    packages=pkgs
-    install_requires = ['daemonize','pyroute2','ipaddress','netifaces']
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
+    packages=pkgs,
+    install_requires = ['daemonize','pyroute2','ipaddress','netifaces'],
+    long_description="See PiCon README",
 )
