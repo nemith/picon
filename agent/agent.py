b56bfcdd72303cd4ac3e33cf0054512dae94f783
Ryan Rawdon <ryan@u13.net>
2016-06-12 18:29:42 +0000

    Updated whitespacing in agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -12,70 +12,70 @@ import glob
 from pyroute2 import IPRoute
 
 def getPorts():
-  # scan for available ports. return a list of port names with /dev/ stripped off
-  ports = glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
-  ports = [ port.replace('/dev/','') for port in ports ]
-  return ports
+    # scan for available ports. return a list of port names with /dev/ stripped off
+    ports = glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
+    ports = [ port.replace('/dev/','') for port in ports ]
+    return ports
 
 def getInterfaceState(ifname):
-  print(ifname)
-  ip = IPRoute()
-  state = ip.get_links(ip.link_lookup(ifname=ifname))[0].get_attr('IFLA_OPERSTATE')
-  ip.close()
-  if state == "UP":
-    return True
-  return False
+    print(ifname)
+    ip = IPRoute()
+    state = ip.get_links(ip.link_lookup(ifname=ifname))[0].get_attr('IFLA_OPERSTATE')
+    ip.close()
+    if state == "UP":
+        return True
+    return False
 def getInterfaces():
-  addrs = {}
-  for iface in netifaces.interfaces():
-    addrs[iface] = {}
-    addrs[iface]['addrs'] = [] 
-    afs = netifaces.ifaddresses(iface)
-    addrs[iface]['state'] = getInterfaceState(iface)
-    for af in afs:
-      if af == netifaces.AF_LINK:
-        continue
-      for addr in afs[af]:
-        # create ipaddress object out of the address
-        # however, link-locals have an interface from netifaces
-        # and ipaddress does not understand that, so we only take the address
-        address = ipaddress.ip_address(addr['addr'].split('%')[0])
+    addrs = {}
+    for iface in netifaces.interfaces():
+        addrs[iface] = {}
+        addrs[iface]['addrs'] = [] 
+        afs = netifaces.ifaddresses(iface)
+        addrs[iface]['state'] = getInterfaceState(iface)
+        for af in afs:
+            if af == netifaces.AF_LINK:
+                continue
+            for addr in afs[af]:
+                # create ipaddress object out of the address
+                # however, link-locals have an interface from netifaces
+                # and ipaddress does not understand that, so we only take the address
+                address = ipaddress.ip_address(addr['addr'].split('%')[0])
 
 	# ipaddress greatly simplifies implementing this logic
-        if address.is_loopback or address.is_link_local:
-          continue
-        addrs[iface]['addrs'].append(str(address))
-  return addrs
+                if address.is_loopback or address.is_link_local:
+                    continue
+                addrs[iface]['addrs'].append(str(address))
+    return addrs
 
 def getHostname():
-  return socket.gethostname()
-  
+    return socket.gethostname()
+    
 def getSerial():
-  # Extract serial from cpuinfo file
-  cpuserial = "0000000000000000"
-  try:
-    f = open('/proc/cpuinfo','r')
-    for line in f:
-      if line[0:6]=='Serial':
-        cpuserial = line[10:26]
-    f.close()
-  except:
-    cpuserial = "ERROR000000000"
+    # Extract serial from cpuinfo file
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo','r')
+        for line in f:
+            if line[0:6]=='Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
  
-  return cpuserial
+    return cpuserial
 
 def main():
 
-  # buffer to assemble the POST body
-  body = {}
-  body['hostname'] = getHostname()  
-  body['sn'] = getSerial()
-  body['interfaces'] = getInterfaces()
-  body['ports'] = getPorts()
-  headers = {'content-type': 'application/json'}
-  jsonbody = json.dumps(body,sort_keys=True,indent=2)
-  requests.post('http://199.187.221.170:5000/api/register', data = jsonbody, headers = headers)
-  print(jsonbody)
+    # buffer to assemble the POST body
+    body = {}
+    body['hostname'] = getHostname()    
+    body['sn'] = getSerial()
+    body['interfaces'] = getInterfaces()
+    body['ports'] = getPorts()
+    headers = {'content-type': 'application/json'}
+    jsonbody = json.dumps(body,sort_keys=True,indent=2)
+    requests.post('http://199.187.221.170:5000/api/register', data = jsonbody, headers = headers)
+    print(jsonbody)
 if __name__ == "__main__":
-  main()
+    main()
 
