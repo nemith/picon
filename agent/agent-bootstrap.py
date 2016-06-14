84931b94bb9d2e6587e2e244c5d1648695b83c72
Ryan Rawdon <ryan@u13.net>
2016-06-13 19:29:06 -0500

    Added some logging to agent

--- agent/agent-bootstrap.py
+++ agent/agent-bootstrap.py
@@ -4,25 +4,46 @@ import argparse
 from daemonize import Daemonize
 import logging
 
-
 parser = argparse.ArgumentParser(description = 'Run a PiCon agent')
 parser.add_argument('endpoint',type=str, help='Base URL of the PiCon server API, e.g. http://picon.example.com/api/')
-parser.add_argument('--daemonize',dest='daemonize',action='store_true',help='Run in background [default: run in foreground]')
+parser.add_argument('--daemonize',action='store_true',help='Run in background [default: run in foreground]')
 parser.add_argument('--holdtime',type=int,help='Hold time: seconds for the server to wait before declaring this device unavailable [default: 300]', default=300)
 parser.add_argument('--interval',type=int,help='Interval: seconds between registrations [default: 60]',default=60)
 parser.add_argument('--pidfile',type=str,help='PID File: PID file location, used only if daemonizing [default: /tmp/picon-agent.pid]',default='/tmp/picon-agent.pid')
+parser.add_argument('--logfile',type=str,help='Log File: log file location, [default: None]',default=None)
+parser.add_argument('-d',dest='debug',action='store_true',help='Debug: Maximum verbosity (overrides -v)')
+parser.add_argument('-v',dest='verbose',action='count',help='Verbose Level: Repeat up to 3 times')
 
 args = parser.parse_args()
 
+def loggingLevelFromVerboseCount(vcount):
+    if vcount is None:
+        return logging.ERROR
+    elif vcount == 1:
+        return logging.WARNING
+    elif vcount == 2:
+        return logging.INFO
+    elif vcount >= 3:
+        return logging.DEBUG
+    else:
+        logger.critical('Undefined Verbosity Level: '+vcount)
+        return logging.ERROR
+
 def main():
+    logLevel = loggingLevelFromVerboseCount(args.verbose)
+    if args.debug:
+        logLevel = logging.DEBUG
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',level=logLevel,filename=args.logfile)
+    logging.info('Starting PiCon Agent...')
     a = PiConAgent(args.endpoint,holdtime=args.holdtime,interval=args.interval)
+    logging.info("Using endpoint %ss, holdtime %ss, reporting interval %s" % (args.endpoint,a.holdtime,a.interval))
     a.run()
 
 
 if args.daemonize:
     daemon = Daemonize(app='picon-agent', pid=args.pidfile, action=main)
     daemon.start()
-    logging.info("Daemonized, PID can be found at %s" % args.pidfile)
+    logging.info("Daemonized, PID (%s) can be found at %s" % args.pidfile)
 else:
     main()
 
--- agent/piconagent/piconagent.py
+++ agent/piconagent/piconagent.py
@@ -1,9 +1,13 @@
+import traceback
 from .utils import *
 from time import sleep
 import logging
 
 class PiConAgent():
     def __init__(self,endpoint='http://localhost/api/',headers={'content-type': 'application/json'},holdtime=300,interval=60):
+        # requests is too noisy for INFO
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger('requests').setLevel(logging.WARN)
         self.endpoint = endpoint
         self.headers = headers
         self.holdtime = holdtime
@@ -16,8 +20,13 @@ class PiConAgent():
         body['ports'] = getPorts()
         body['holdtime'] = self.holdtime
         jsonbody = json.dumps(body,sort_keys=True,indent=2)
-        print(self.endpoint+'register')
-        requests.post(self.endpoint+'register', data = jsonbody, headers = self.headers,timeout=2)
+        try:
+            requests.post(self.endpoint+'register', data = jsonbody, headers = self.headers,timeout=2)
+        except Exception as e:
+            logging.error('PiCon registration attempt failed: ' + str(e))
+        else:
+            logging.debug("Using registration endpoint %s, send JSON %s" % (self.endpoint+'register',jsonbody))
+
     def run(self):
         while True:
             self.register()
